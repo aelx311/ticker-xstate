53aa4bd7540bf8577e78583156925b7629734d43
Alex Dehe Ng <alex311@gmail.com>
2021-10-17 00:40:25 -0500

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,128 +1,59 @@
 import './styles.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { createMachine, assign } from 'xstate';
 import { useMachine } from '@xstate/react';
 import Button from '@mui/material/Button';
-import Input from '@mui/material/Input';
 import Grid from '@mui/material/Grid';
 import CircularProgress from '@mui/material/CircularProgress';
 import { Table } from './table';
 import Box from '@mui/material/Box';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
-import Typography from '@mui/material/Typography';
-
-interface TickerContext {
-  ticker: string;
-  data: Array<any>;
-  error: any;
-}
-
-function invokeFetchTicker(context) {
-  const { ticker } = context;
-
-  return fetch(
-    `https://data.nasdaq.com/api/v3/datasets/BATS/EDGA_${ticker}?start_date=2021-01-01&end_date=2021-12-31&api_key=2dzi5Bc538dC8dYGhjts`,
-  )
-    .then((response) => {
-      if (!response.ok) {
-        return Promise.reject('error getting data');
-      }
-
-      return response.json();
-    })
-    .then((json) => json.dataset);
-}
-
-const tickerMachine = createMachine<TickerContext>({
-  id: 'ticker',
-  initial: 'idle',
-  context: {
-    ticker: '',
-    data: [],
-    error: null,
-  },
-  states: {
-    idle: {
-      on: {
-        FETCH: {
-          target: 'loading',
-          cond: (context) => context.ticker.length > 0,
-        },
-      },
-    },
-    loading: {
-      invoke: {
-        id: 'loading-ticker',
-        src: invokeFetchTicker,
-        onDone: {
-          target: 'loaded',
-          actions: assign({
-            data: (context, event) => event.data,
-          }),
-        },
-        onError: {
-          target: 'failed',
-          actions: assign({
-            error: (context, event) => event.data,
-          }),
-        },
-      },
-    },
-    loaded: {
-      on: {
-        FETCH: {
-          target: 'loading',
-          cond: (context) => context.ticker.length > 0,
-        },
-      },
-    },
-    failed: {
-      on: {
-        FETCH: {
-          target: 'loading',
-          cond: (context) => context.ticker.length > 0,
-        },
-        RESET: {
-          target: 'idle',
-        },
-      },
-    },
-  },
-  on: {
-    TICKER_INPUT: {
-      actions: assign({
-        ticker: (context, event) => event.ticker,
-      }),
-    },
-  },
-});
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import tickerMachine from './machine';
 
 function App() {
   const [current, send] = useMachine(tickerMachine);
-  const { ticker, data, error } = current.context;
+  const { ticker, data, exchange, error } = current.context;
 
   return (
     <div className="App">
-      <Typography variant="h2" gutterBottom component="div">
-        Ticker Machine
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={12} lg={12}>
-          <Input
-            type="text"
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        <Grid item xs={4}>
+          <TextField
+            label="Ticker"
+            variant="outlined"
             onChange={(event) =>
-              send('TICKER_INPUT', { ticker: event.target.value })
+              send('TICKER_INPUT', { ticker: event.target.value as string })
             }
           />
         </Grid>
-        <Grid item xs={12} md={12} lg={12}>
+        <Grid item xs={4}>
+          <TextField
+            select
+            value={exchange}
+            label="Exchange"
+            variant="outlined"
+            onChange={(event) =>
+              send('EXCHANGE_SELECT', {
+                exchange: event.target.value as string
+              })
+            }
+          >
+            <MenuItem value="BATS">BATS</MenuItem>
+            <MenuItem value="BYXX">BYXX</MenuItem>
+            <MenuItem value="EDGA">EDGA</MenuItem>
+            <MenuItem value="EDGX">EDGX</MenuItem>
+          </TextField>
+        </Grid>
+        <Grid item xs={4}>
           <Button
             variant="contained"
-            disabled={ticker.length === 0}
-            onClick={() => send('FETCH')}>
-            Click me to fetch
+            disabled={ticker.length === 0 || exchange.length === 0}
+            onClick={() => send('FETCH')}
+          >
+            Fetch
           </Button>
         </Grid>
       </Grid>
@@ -136,7 +67,8 @@ function App() {
           open={true}
           autoHideDuration={6000}
           message={error}
-          onClose={() => send('RESET')}>
+          onClose={() => send('RESET')}
+        >
           <Alert severity="error">{error}</Alert>
         </Snackbar>
       )}
